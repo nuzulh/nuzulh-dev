9be2d93fafedb5c30df4415cda2f23762ee50a86
nuzulh <nuzulhikam0@gmail.com>
2024-07-26 21:40:38 +0700

    chore: about metadata & install deps @radix-ui/react-accordion

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,6 +1,13 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'About - Nuzul H.',
+  description: 'About me.',
+};
+
 export default function AboutPage() {
   return (
-    <main>
+    <main data-aos='fade-up'>
       About
     </main>
   );
