6aa3ca0973baa1f1daf99d1b17c99fd1c02bbe34
nuzulh <nuzulhikam0@gmail.com>
2024-07-27 17:31:07 +0700

    fix: adjust download resume button visibility

--- src/components/organisms/TabsAbout/contents/resume.tsx
+++ src/components/organisms/TabsAbout/contents/resume.tsx
@@ -3,19 +3,29 @@ import { Card, CardHeader, TabsContent } from '@/components/molecules';
 import { Download } from 'lucide-react';
 import Link from 'next/link';
 
+function DownloadButton() {
+  return (
+    <Atoms.Button variant='outline' asChild>
+      <Link href='/docs/resume.pdf' target='_blank'>
+        <Download className='size-4 mr-2' /> Download my resume
+      </Link>
+    </Atoms.Button>
+  );
+}
+
 export function TabsContentResume() {
   return (
     <TabsContent value='resume'>
-      <Card className='bg-card hidden lg:block'>
-        <CardHeader data-aos='fade-left'>
+      <Card className='bg-card'>
+        <CardHeader data-aos='fade-left' className='hidden lg:block space-y-4'>
+          <DownloadButton />
           <iframe src='/docs/resume.pdf' className='w-full h-dvh' />
         </CardHeader>
+
+        <CardHeader data-aos='fade-left' className='lg:hidden'>
+          <DownloadButton />
+        </CardHeader>
       </Card>
-      <Atoms.Button data-aos='fade-left' className='flex lg:hidden w-full' asChild>
-        <Link href='/docs/resume.pdf' target='_blank'>
-          <Download className='size-4 mr-2' /> Download my resume
-        </Link>
-      </Atoms.Button>
     </TabsContent>
   );
 }
