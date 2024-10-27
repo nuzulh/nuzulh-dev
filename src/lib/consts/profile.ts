6f88c38d15dfb839213d1f8e85e10aad06d76f20
nuzulh <nuzulhikam0@gmail.com>
2024-10-27 20:07:20 +0700

    chore: shorten profile

--- src/lib/consts/profile.ts
+++ src/lib/consts/profile.ts
@@ -8,7 +8,7 @@ export const MY_PROFILE: Profile = {
   avatar: 'https://github.com/nuzulh.png',
   address: 'Banda Aceh, Indonesia 🇮🇩',
   status1: 'Working remotely',
-  description: 'Software Engineer with 3+ years of expertise in crafting cutting-edge web and mobile applications, specializing in frontend development. Skilled in creating intuitive user interfaces, optimizing performance, and leveraging the latest technologies. Known for meticulous attention to detail, creative problem-solving, and a passion for staying ahead of industry trends. Committed to delivering high-impact, user-centric solutions that not only meet but exceed client expectations, driving business growth and elevating user experiences. Adept at collaborating with cross-functional teams to transform complex requirements into seamless, efficient, and visually appealing applications.',
+  description: 'Frontend Engineer with 3+ years of experience specializing in frontend development for web and mobile applications. Skilled in creating intuitive UIs, optimizing performance, and staying current with industry trends. Committed to delivering user-centric solutions that exceed expectations.',
   socials: [
     { label: 'Linkedin', href: 'https://linkedin.com/in/nuzulh', icon: Linkedin },
     { label: 'GitHub', href: 'https://github.com/nuzulh', icon: Github },
